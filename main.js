17d2497cc10ced6785ef741a4cfc46ee2fe6b70b
emrekayik <emrekayik1905@gmail.com>
2023-01-01 22:59:36 +0300

    kodlar yazıldı, bütünüyle

--- main.js
+++ main.js
@@ -1,21 +1,3 @@
-/**
- * This pen allows you to use all your favourite console functions right in
- * CodePen: `console.log`, `console.info`, `console.warn`, `console.error`,
- * and `console.clear` are supported.
- *
- * To scroll the console to the bottom as messages are printed use the
- * `console.follow` function.
- *
- * Fork this pen to quickly create a new pen which is pre-configured for
- * JavaScript console output.
- *
- * If you want to learn how this works then take a look at:
- *
- * https://codepen.io/nullobject/pen/cngzI
- * 
- * Made with love by @nullobject (http://joshbassett.info), 2014.
- */
-
 // Log an object.
 console.log({a: 'foo', b: 'bar'});
 
